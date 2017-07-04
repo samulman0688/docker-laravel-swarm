07ec84743ebdffd298439e71eecd59eb3bbf9fac
glow@socar.kr <glow@socar.kr>
2017-07-05 00:38:15 +0900

    헬첵 추가

--- docker-php-update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker-compose -f docker-compose.live.yml build php
-docker-compose -f docker-compose.live.yml push php
-docker service update --image samulman0688/laravel-app --detach=false laravel_php
